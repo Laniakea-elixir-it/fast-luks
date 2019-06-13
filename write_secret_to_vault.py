e33dad89905ac9d30e2b479704bcc27113abedb4
mtangaro <ma.tangaro@gmail.com>
2019-06-13 11:56:01 +0000

    fixes

---
+++ write_secret_to_vault.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+"""
+"""
+
+import os
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+import argparse
+import json
+
+logfile = '/tmp/readdb.log'
+
+#______________________________________
+def cli_options():
+  parser = argparse.ArgumentParser(description='Vault connector')
+  parser.add_argument('-v', '--vault-url', dest='vault_url', help='Vault endpoint')
+  parser.add_argument('-w', '--wrapped-token', dest='wrapped_token', help='Wrapped Token')
+  parser.add_argument('-p', '--secret-path', dest='secret_path', help='Secret path on vault')
+  parser.add_argument('--key', dest='user_key', default='luks', help='Vault user key value, i.e. passphrase')
+  parser.add_argument('--value', dest='user_value', help='Vault user key')
+  return parser.parse_args()
+
+
+#______________________________________
+def unwrap_vault_token(url, wrapped_token):
+
+  url = url + '/v1/sys/wrapping/unwrap'
+
+  headers = { "X-Vault-Token": wrapped_token }
+
+  response = requests.post(url, headers=headers, verify=False)
+
+  response.raise_for_status()
+
+  deserialized_response = json.loads(response.text)
+
+  try:
+    deserialized_response["auth"]["client_token"]
+  except KeyError:
+    raise Exception("[FATAL] Unable to unwrap vault token.")
+
+  return deserialized_response["auth"]["client_token"]
+
+#______________________________________
+def post_secret(url, path, token, key, value):
+
+  url = url + '/v1/secrets/data/' + path
+
+  headers = { "X-Vault-Token": token }
+
+  data = '{ "options": { "cas": 0 }, "data": { "'+key+'": "'+value+'"} }'
+
+  response = requests.post(url, headers=headers, data=data, verify=False)
+
+  response.raise_for_status()
+
+  deserialized_response = json.loads(response.text)
+    
+  try:
+    deserialized_response["data"]
+  except KeyError:
+    raise Exception("[FATAL] Unable to write vault path.")
+
+  return deserialized_response
+
+#______________________________________
+def parse_response(response):
+
+  if not response["data"]["created_time"]:
+    raise Exception("No cretation time")
+
+  if response["data"]["destroyed"] != False:
+    raise Exception("Token already detroyed")
+
+  if response["data"]["version"] != 1:
+    raise Exception("Token not at 1st verion")
+
+  if response["data"]["deletion_time"] != "":
+    raise Exception("Token aready deleted")
+
+  return 0
+
+#______________________________________
+def revoke_token(url, token):
+
+  url = url + '/v1/auth/token/revoke-self'
+
+  headers = { "X-Vault-Token": token }
+
+  response = requests.post( url, headers=headers, verify=False )
+
+#______________________________________
+def write_secret_to_vault():
+
+  options = cli_options()
+
+  # Check vault
+  r = requests.get(options.vault_url)
+  r.raise_for_status()
+
+  # Write the secret
+  write_token = unwrap_vault_token( options.vault_url, options.wrapped_token )
+
+  response_output = post_secret( options.vault_url, options.secret_path, write_token, options.user_key, options.user_value )
+
+  parse_response( response_output )
+
+  revoke_token( options.vault_url, write_token )
+
+#______________________________________
+if __name__ == '__main__':
+  write_secret_to_vault()
